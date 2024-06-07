10e4d7936eae8271528d6834e8c157fe2a3bbcb4
LukaMakharadze41 <lukamakharadze41@gmail.com>
2024-06-07 11:07:27 +0400

    commit

---
+++ homework/goa homeworck#3.py
@@ -0,0 +1,20 @@
+# დავალება  N1
+
+name = 'luka'
+last_name = 'makharadze'
+age = 11
+hobi ='basketball'
+countre = 'georgia'
+
+# პრინტის პირველი ვერსია!!
+
+print(name , last_name , age , hobi , countre)
+
+# დავალება N1
+
+name = 'luka'
+last_name = 'makharadze'
+age = 11
+hobi ='basketball'
+countre = 'georgia' 
+print(name+" " +last_name+" "+str(age)+" "+hobi+" "+countre)
