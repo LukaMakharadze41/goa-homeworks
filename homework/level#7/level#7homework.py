461f128d58350f04701e344f26aa3b08f1f9cc0c
LukaMakharadze41 <lukamakharadze41@gmail.com>
2024-06-28 19:04:03 +0400

    commit

---
+++ homework/level#7/level#7homework.py
@@ -0,0 +1,44 @@
+#1
+
+a = input("enter 'a' number:")
+b = input("enter 'b' number:")
+c = input("enter 'c' number:")
+d = input("enter 'd' number:")
+
+print(a + b + c + d)
+
+#2
+l=input('enter your mothers age:')
+m=input('enter your fathers age:')
+q=input('enter your grandmothers age:')
+j=input('enter your grandfathers age:')
+
+print(l+str(20))
+print(m+str(20))
+print(q+str(20))
+print(j+str(20))
+
+#3
+name ='luka'
+last_name='maxaradze'
+age=11
+qountry='georgia'
+city='tbillisi'
+favorite_food='eg with brad'
+favorite_car='tesla and mercedec'                
+favorite_sport='basketball and football'
+favorite_color='rec,black,white and bloo'
+
+print(name +' '+last_name +' ' + str(age) +' '+ qountry +' ' + city + ' ' + favorite_food +' ' +favorite_car +' ' + favorite_sport + ' ' + favorite_color)
+
+# 4
+input('user enter your name:')
+
+input('user enter congrutalation by using ur name:')
+
+#5
+
+კრუბიკრუბიკი=5
+
+რაონდენობა=36
+print(კრუბიკრუბიკი *  რაონდენობა)
