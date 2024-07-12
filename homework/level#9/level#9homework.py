10a2f58c489b3981c26443d5dd8d7cc60648999d
LukaMakharadze41 <lukamakharadze41@gmail.com>
2024-07-12 13:52:08 +0400

    commit

---
+++ homework/level#9/level#9homework.py
@@ -0,0 +1,63 @@
+#2
+
+print(5>4)
+print(8>5)
+print(15==15)
+print(2>1)
+print(15>0)
+print(0>1)
+print(20>9)
+print(5<=6)
+print(5>7)
+print(1000000000==1000000000)
+print(1<0.9)
+print(100>20)
+print(100>=101)
+print(1000>999)
+print(999==998)
+print(1000>2000)
+print(1000<10000)
+print(5>=5)
+print(777>778)
+print(1>1)
+print(0>0)
+print(0>=1)
+print(1==1)
+print(10000000000000>10000000000000)
+print(1111111111+9>1111111111)
+print(123456789<987654321)
+print(0.00000000000000000000000000000000001>0.0000000000000000000000000000000000000000000000000000000001)
+print(123.000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001<123456789)
+
+#3
+
+savings=input('enter the savings')
+balance=savings
+message=balance
+print(message)
+
+#4
+
+f_score=int(input(120<95))
+s_score=int(input(95>120))
+
+#5
+
+savings=input('enter savings')
+balance= savings * 1.05
+message= "amount in 1  year:"
+print(balance)
+
+#6
+
+wins= input('1 win 3 point there are 3 wins')
+ties= input('1 tie= 1 point there are 2 ties=' )
+
+# 1 win =3 point
+# 1 tie= 1 point 
+# calculate the score
+score=wins + ties
+print(int(wins)+int(ties))
+messsage="score:"           
+str(score)
+
