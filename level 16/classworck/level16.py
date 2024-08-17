5f1ac03cf9ba21e8c5918ca131a95252ab54b094
LukaMakharadze41 <167802193+LukaMakharadze41@users.noreply.github.com>
2024-08-17 18:54:02 +0400

    Add files via upload

---
+++ level 16/classworck/level16.py
@@ -0,0 +1,135 @@
+from  turtle import *
+left(30)
+forward(100)
+right(60)
+forward(100)
+right(60)
+forward(100)
+right(60)
+forward(100)
+right(60)
+forward(100)
+right(60)
+forward(100)
+
+#====================================
+
+bill=print(50*20/100)
+
+#===================================
+
+print(7425/550)
+
+#==================================
+
+print(24*60*60)
+print(type(input(print)))
+
+#==================================
+
+print(9*9-2)
+int(input("how many will this operation output:9*9-2="))
+
+#====================================
+
+
+
+print(int(input("enter ur nikname:")))
+print(int(input("enter ur score:")))
+
+#====================================
+
+print(str(input("countre:")))
+print(str(input("ccapital:")))
+
+#=====================================
+
+Wins=int(input("enter ur wins:"))
+ties=int(input("enter ur ties:"))
+
+#======================================
+
+balance=int((input("enter ur balance:")))
+withdraw=int(input("enter ur withdraw:"))
+
+#====================================#====================================
+
+bill=print(50*20/100)
+
+#===================================
+
+print(7425/550)
+
+#==================================
+
+print(24*60*60)
+print(type(input(print)))
+
+#==================================
+
+print(9*9-2)
+int(input("how many will this operation output:9*9-2="))
+
+#====================================
+
+
+
+print(int(input("enter ur nikname:")))
+print(int(input("enter ur score:")))
+
+#====================================
+
+print(str(input("countre:")))
+print(str(input("ccapital:")))
+
+#=====================================
+
+Wins=int(input("enter ur wins:"))
+ties=int(input("enter ur ties:"))
+
+#======================================
+
+balance=int((input("enter ur balance:")))
+withdraw=int(input("enter ur withdraw:"))
+
+#====================================#====================================
+
+bill=print(50*20/100)
+
+#===================================
+
+print(7425/550)
+
+#==================================
+
+print(24*60*60)
+print(type(input(print)))
+
+#==================================
+
+print(9*9-2)
+int(input("how many will this operation output:9*9-2="))
+
+#====================================
+
+
+
+print(int(input("enter ur nikname:")))
+print(int(input("enter ur score:")))
+
+#====================================
+
+print(str(input("countre:")))
+print(str(input("ccapital:")))
+
+#=====================================
+
+Wins=int(input("enter ur wins:"))
+ties=int(input("enter ur ties:"))
+
+#======================================
+
+balance=int((input("enter ur balance:")))
+withdraw=int(input("enter ur withdraw:"))
+
+#====================================
---
+++ level 16/homeworck/level 16.py
@@ -0,0 +1,28 @@
+#1============================================
+
+name="luka"
+last_name="makharadze"
+print(name +" "+ last_name)
+
+# 2===================================
+
+age=int(input("enter ur age:"))
+name=str(input("enter ur name:"))
+last_name_2=str(input("enter ur lasrname"))
+fav_num=int(input(("enter ur favorite num:")))
+fav_num_2=int(input("enter ur second favorite num:"))
+
+# 3============================
+
+print(name + " " + last_name_2)
+print(fav_num+fav_num_2)
+
+# 4===============================
+
+print(fav_num==fav_num_2)
+
+#5==============================
+
+age_2=11
+print(age or age_2)
+print(age and age_2)
