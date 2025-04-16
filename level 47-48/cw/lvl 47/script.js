4a94d5776e52d1afdfc3623d1fbc8e5215be7ee1
luka makharadze <167802193+LukaMakharadze41@users.noreply.github.com>
2025-04-16 19:17:20 +0400

    Add files via upload

---
+++ level 47-48/cw/lvl 47/script.js
@@ -0,0 +1,5 @@
+1= alert("enter ur name:");
+2=alert("enter ur last_name")
+3=alert("enter ur age")
+4=alert("enter ur home address")
+alert(1 +"," + 2 + "," + 3 + "," + 4)
---
+++ level 47-48/hw/script.js
@@ -0,0 +1,74 @@
+let name= prompt("Enter your name:");
+let age= prompt("Enter your age:");
+let lastName= prompt("Enter your last name:");
+let home_point= prompt("Enter your home point:");
+let mom_name= prompt("Enter your mom name:");
+let mom_lastName= prompt("Enter your mom last name:");
+let mom_age= prompt("Enter your mom age:");
+let dad_name= prompt("Enter your dad name:");
+let dad_lastName= prompt("Enter your dad last name:");
+let dad_age= prompt("Enter your dad age:");
+let brother_name= prompt("Enter your brother name:");
+let brother_lastName= prompt("Enter your brother last name:");
+let brother_age= prompt("Enter your brother age:");
+
+alert("\n ur name is:" + name + "\n ur age is:" + age + "\n ur last name is:" + lastName + "\n ur home point is:" + home_point + "\n ur mom name is:" + mom_name + "\n ur mom last name is:" + mom_lastName + "\n ur mom age is:" + mom_age + "\n ur dad name is:" + dad_name + "\n ur dad last name is:" + dad_lastName + "\n ur dad age is:" + dad_age + "\n ur brother name is:" + brother_name + "\n ur brother last name is:" + brother_lastName + "\n ur brother age is:" + brother_age);
+console.log("\n ur name is:" + name + "\n ur age is:" + age + "\n ur last name is:" + lastName + "\n ur home point is:" + home_point + "\n ur mom name is:" + mom_name + "\n ur mom last name is:" + mom_lastName + "\n ur mom age is:" + mom_age + "\n ur dad name is:" + dad_name + "\n ur dad last name is:" + dad_lastName + "\n ur dad age is:" + dad_age + "\n ur brother name is:" + brother_name + "\n ur brother last name is:" + brother_lastName + "\n ur brother age is:" + brother_age)
+
+
+
+
+
+
+// task 2
+
+//დავალება: გამოთვალეთ დარჩენილი ვაშლი
+//თქვენ გაქვთ ვაშლის კალათა და თქვენს მეგობარს აძლევთ. პროგრამა იღებს ორ რიცხვს, როგორც შეყვანას, ვაშლის საერთო რაოდენობას და ვაშლის რაოდენობას, რომელსაც თქვენ აძლევთ და ანიჭებს მათ შესაბამის total_apples და given_apples ცვლადებს.
+
+switch(prompt("enter your total apples number")) {
+    case '0':
+        alert("you have no apples")
+        break;
+    case '10':
+        alert("you have ten apple")
+        break;
+    case '20':
+        alert("you have tweny apples")
+        break;
+    case '30':
+        alert("you have thirty apples")
+        break;
+    case '40':
+        alert("you have forty apples")
+        break;
+    case '50':
+        alert("you have fifty apples")
+        break;
+    case '60':
+        alert("you have sixty apples")
+        break;
+    case '70':
+        alert("you have seventy apples")
+        break;
+    case '80':
+        alert("you have eighty apples")
+        break;
+    case '90':
+        alert("you have ninety apples")
+        break;
+    case '100':
+        alert("you have hundred apples")
+        break;
+    default:
+        alert("bro that many aples you cant have")
+        break;
+}
+
+//დავალება: ანაბარი ბანკში
+//თჟკაქ ეა ჟოაჟთქ ოაპთ. პროგრამა იღებს ორ ნომერს, როგორც შეყვანას, თქვენს თავდაპირველ დანაზოგებს და თანხას, რომლის დამატებასაც აპირებთ და ანიჭებს მათ შესაბამის დანაზოგებსა და დეპოზიტების ცვლადებს.
+
+
+
+//დავალება: მონეტების კოლექცია
+//თქვენ გაქვთ მონეტების კოლექცია. პროგრამა იღებს ორ რიცხვს, როგორც შეყვანას, მონეტების საერთო რაოდენობას და თქვენ მიერ დახარჯული მონეტების რაოდენობას და ანიჭებს მათ შესაბამის total_coins და spent_coins ცვლადებს.
+
