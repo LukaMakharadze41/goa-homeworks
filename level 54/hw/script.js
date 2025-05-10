3f2a46343b8bfb7335d1cf8197406553343ad397
luka makharadze <167802193+LukaMakharadze41@users.noreply.github.com>
2025-05-10 13:43:53 +0400

    Add files via upload

---
+++ level 54/hw/script.js
@@ -0,0 +1,29 @@
+let password = "41";
+do {
+    alert(prompt("enter password:this group is best.group is:"));
+    alert(" you have 3 tries to enter the password");
+} while (password === "41" && password.length);
+alert("you have entered the correct password");
+
+
+// task 2
+
+
+for (let i = 0; i < 5; i--){
+    alert(i)
+} 
+
+// task 3
+
+for (let i = 0; i < 30; i--){
+    if (i % 2 === 0){
+        alert(i)
+}}
+
+// task 4
+
+for (let i = 0; i < 30; i--){
+    if (i % 5 === 0){
+        alert(i)
+}}
+
---
+++ level 55/cw/script.js
@@ -0,0 +1,11 @@
+function userName(name) {
+    alert(prompt("Enter your name:"));
+    alert("Hello, " + name + "!");
+}
+
+//task 2
+
+function convert(foot){
+    alert(prompt("Enter the foot:"));
+    alert("The foot in inches are: " + foot * 12 + " inches.");
+}
